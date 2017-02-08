c02d3b121fc32be49a0d4a3f7f289f1bd1f43ad9
Zach Wade <zacharywade@Zachs-MBP.wv.cc.cmu.edu>
2017-02-08 10:29:00 -0500

    light triggering

--- ayla.js
+++ ayla.js
@@ -1,18 +1,35 @@
 const request = require("request");
 
-module.exports = function(auth, on) {
-	request.post({
-		url: "https://ads-field.aylanetworks.com/apiv1/properties/18944260/datapoints.json",
-		headers: {
-			"Authorization": `auth_token ${auth}`,
-			"Content-Type": "application/json"
-		},
-		json: true,
-		body: {
-			"datapoint": {
-				"metadata": null,
-				"value": on ? 1 : 0
+class ayla {
+	constructor() {
+		this.state = 0;
+	}
+
+	setState(auth, on) {
+		this.state = on
+		request.post({
+			url: "https://ads-field.aylanetworks.com/apiv1/properties/18944260/datapoints.json",
+			headers: {
+				"Authorization": `auth_token ${auth}`,
+				"Content-Type": "application/json"
+			},
+			json: true,
+			body: {
+				"datapoint": {
+					"metadata": null,
+					"value": on ? 1 : 0
+				}
 			}
+		});
+	}
+
+	trigger(auth) {
+		return () => {
+			this.state = this.state ^ 1;
+			this.setState(auth, this.state);
+			return this.state ? "On" : "Off";
 		}
-	});
+	}
 }
+
+module.exports = ayla;
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const nconf = require("nconf");
 nconf.argv().env();
 
 const dash = require("./dash");
-const ayla = require("./ayla");
+const aylaT = require("./ayla");
 const cast = require("./cast");
 const particle = require("./particle")(nconf.get("PARTICLE_USER"), nconf.get("PARTICLE_PASS"));
 const delay = require("./delay");
@@ -16,16 +16,23 @@ const process = require("process");
 
 const file = new static.Server("./public");
 
+const ayla = new aylaT();
+
 http.createServer((req, res) => {
 	req.addListener("end", () => {
 		if (req.method === "POST" && req.url === "/lights/on") {
-			ayla(nconf.get("AYLA_AUTH_TOKEN"), true);
+			ayla.setState(nconf.get("AYLA_AUTH_TOKEN"), true);
 			res.write("On");
 			res.end();
 			return;
 		} else if (req.method === "POST" && req.url === "/lights/off") {
-			ayla(nconf.get("AYLA_AUTH_TOKEN"), false);
+			ayla.setState(nconf.get("AYLA_AUTH_TOKEN"), false);
 			res.write("Off");
+			res.end();
+			return;
+		} else if (req.method === "POST" && req.url === "/lights/trigger") {
+			let state = ayla.trigger(nconf.get("AYLA_AUTH_TOKEN"))();
+			res.write(state);
 			res.end();
 			return;
 		}
@@ -44,5 +51,6 @@ http.createServer((req, res) => {
 	}).resume();
 }).listen(8080);
 
-dash("a0:02:dc:3a:1c:ec", "ON"); // Do nothing; used for local testing
+console.log(ayla)
+dash("a0:02:dc:3a:1c:ec", "ON", ayla.trigger(nconf.get("AYLA_AUTH_TOKEN")));
 dash("44:65:0d:c5:6d:5f", "All", cast("doorbell.mp3", "audio/mp3", .75, 12000), particle("1f0039001047343339383037", "setPattern", "flash"), delay(particle("1f0039001047343339383037", "setPattern", "last"), 12000));
