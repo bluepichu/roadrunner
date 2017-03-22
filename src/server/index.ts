c60fa1e382ba45f7233a2a092ea39e46d94f365c
Zach Wade <zacharywade@Zachs-MacBook-Pro.local>
2017-03-22 14:28:39 -0400

    cleaned up get request

--- src/server/index.ts
+++ src/server/index.ts
@@ -42,7 +42,7 @@ handle.POST("/lights/on", () => ayla.setState(aylaAuth, true))
 handle.POST("/lights/off", () => ayla.setState(aylaAuth, false))
 handle.POST("/lights/trigger", () => ayla.trigger(aylaAuth))
 
-handle['GET']("/lights/trigger", () => ayla.trigger(aylaAuth))
+handle.GET("/lights/trigger", () => ayla.trigger(aylaAuth))
 
 dash("a0:02:dc:3a:1c:ec", "ON", () => ayla.trigger(aylaAuth));
 dash("44:65:0d:c5:6d:5f", "All", cast("doorbell.mp3", "audio/mp3", .75, 12000), particle("1f0039001047343339383037", "setPattern", "flash"), delay(particle("1f0039001047343339383037", "setPattern", "last"), 12000));
