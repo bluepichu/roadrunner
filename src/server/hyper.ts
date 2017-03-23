2abb11a19feb903086695462e690cba4aee3e9cc
bluepichu <github@bluepichu.com>
2017-03-22 22:17:10 -0400

    Just export express

--- src/server/hyper.ts
+++ src/server/hyper.ts
@@ -1,74 +1,19 @@
-import * as http from "http"
 import * as log from "beautiful-log"
-import * as staticServer from "node-static"
-import * as cp from "child_process"
+import * as cp from "child_process";
+import * as express from "express";
 
-const file = new staticServer.Server("./public");
+let app = express();
 
-class HyperAct {
-	methods:Map<string, Map<string, ((req: http.ServerRequest, res: http.ServerResponse) => void)>> = new Map()
-	constructor() {
-		let ha = this
+app.use(express.static("../../public"));
 
-		http.createServer((req, res) => {
-			req.addListener("end", () => {
-				if (ha.methods.has(req.method) && ha.methods.get(req.method).has(req.url)) {
-					ha.methods.get(req.method).get(req.url)(req, res)
-				}
-				
+app.post("/git-update", (req, res) => {
+	log.ok("Received git update");
+	res.writeHead(200, "OK");
+	res.end();
+	cp.execSync("su runner -c 'git reset --hard HEAD && git pull -f origin master && npm install'");
+	process.exit();
+});
 
-				if (req.method === "POST" && req.url === "/git-update") {
-					log.ok("Received git update");
-					res.writeHead(200, "OK");
-					res.end();
-					cp.execSync("su runner -c 'git reset --hard HEAD && git pull -f origin master && npm install'");
-					process.exit();
-				} else {
-					log.log("Serving:", req.url);
-					file.serve(req, res);
-				}
-			}).resume();
-		}).listen(8080);
-	}
-}
+app.listen(8080);
 
-export interface methods {
-	[name:string]: (url: string, ...opts: (() => any)[]) => any
-	POST:          (url: string, ...opts: (() => any)[]) => any
-	PUT:           (url: string, ...opts: (() => any)[]) => any
-	GET:           (url: string, ...opts: (() => any)[]) => any
-	HEAD:          (url: string, ...opts: (() => any)[]) => any
-	DELETE:        (url: string, ...opts: (() => any)[]) => any
-	OPTIONS:       (url: string, ...opts: (() => any)[]) => any
-	CONNECT:       (url: string, ...opts: (() => any)[]) => any
-}
-
-let target = new HyperAct()
-
-export let handler: methods = new Proxy({}, {
-	"get": function(t: any, name: string | number | symbol): any {
-		let field = name.toString()
-		if (!target.methods.get(field)) {
-			target.methods.set(field, new Map())
-		}
-
-		return function(url: string, ...opts: (() => any)[]) {
-			target.methods.get(field).set(url, function(req, res) {
-				try {
-					let out = ""
-					for (let fn of opts) {
-						let res = fn()
-						if (typeof res === "string") {
-							out += res
-						}
-					}
-					res.writeHead(200, "Ok")
-					res.end(out)
-				} catch (e) {
-					res.writeHead(500, "Dunno Mate")
-					res.end("Failed!")
-				}
-			})
-		}
-	}
-})
+export default app;
--- src/server/index.ts
+++ src/server/index.ts
@@ -15,13 +15,11 @@ import aylaT from "./ayla"
 import cast from "./cast"
 import particleT from "./particle"
 import delay from "./delay"
-import * as hyper from "./hyper" 
+import app from "./hyper"
 
 import * as log from "beautiful-log"
 import * as process from "process"
 import * as schedule from "node-schedule"
-
-let handle = hyper.handler as hyper.methods
 
 const ayla     = new aylaT();
 const particle = particleT(nconf.get("PARTICLE_USER"), nconf.get("PARTICLE_PASS"));
@@ -34,15 +32,10 @@ let renew = () =>
 schedule.scheduleJob('0 0 4 * * *', renew)
 renew()
 
-
-delay(() => "boo", 12)
-
-
-handle.POST("/lights/on", () => ayla.setState(aylaAuth, true))
-handle.POST("/lights/off", () => ayla.setState(aylaAuth, false))
-handle.POST("/lights/trigger", () => ayla.trigger(aylaAuth))
-
-handle.GET("/lights/trigger", () => ayla.trigger(aylaAuth))
+app.post("/lights/on", (req, res) => { ayla.setState(aylaAuth, true); res.status(200).send("Ok"); })
+app.post("/lights/off", (req, res) => { ayla.setState(aylaAuth, false); res.status(200).send("Ok"); })
+app.post("/lights/trigger", (req, res) => { ayla.trigger(aylaAuth); res.status(200).send("Ok"); })
+app.get("/lights/trigger", (req, res) => { ayla.trigger(aylaAuth); res.status(200).send("Ok"); })
 
 dash("a0:02:dc:3a:1c:ec", "ON", () => ayla.trigger(aylaAuth));
 dash("44:65:0d:c5:6d:5f", "All", cast("doorbell.mp3", "audio/mp3", .75, 12000), particle("1f0039001047343339383037", "setPattern", "flash"), delay(particle("1f0039001047343339383037", "setPattern", "last"), 12000));
