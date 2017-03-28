d84fa4865d4af81539dfafebc539ccd075b2bee1
Zachary Wade <zacharywade@gmail.com>
2017-03-28 12:50:14 -0400

    add local --TEST option

--- src/server/hyper.ts
+++ src/server/hyper.ts
@@ -2,7 +2,10 @@ import * as log from "beautiful-log"
 import * as cp from "child_process";
 import * as express from "express";
 import * as path from "path";
+import * as nconf from "nconf";
 import greenlock = require("greenlock-express")
+
+nconf.argv().env()
 
 let app = express();
 
@@ -16,12 +19,17 @@ app.post("/git-update", (req, res) => {
 	process.exit();
 });
 
-greenlock.create({
-	server: "https://acme-v01.api.letsencrypt.org/directory",
-	email: "zwade@dttw.tech",
-	agreeTos: true,
-	approveDomains: ["local.ctfit.pw"],
-	app: app
-}).listen(8080, 8081)
+
+if (!nconf.get("TEST")) {
+	greenlock.create({
+		server: "https://acme-v01.api.letsencrypt.org/directory",
+		email: "zwade@dttw.tech",
+		agreeTos: true,
+		approveDomains: ["local.ctfit.pw"],
+		app: app
+	}).listen(8080, 8081)
+} else {
+	app.listen(8080)
+}
 
 export default app;
