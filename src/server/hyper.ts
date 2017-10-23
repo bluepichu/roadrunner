239e4113cca1c1b6b8ab516c4207dac30fff5143
Zachary Wade <zacharywade@gmail.com>
2017-10-23 19:56:54 -0400

    should pull correctly now?

--- src/server/hyper.ts
+++ src/server/hyper.ts
@@ -15,7 +15,7 @@ app.post("/git-update", (req, res) => {
 	log.ok("Received git update");
 	res.writeHead(200, "OK");
 	res.end();
-	cp.execSync("su runner -c 'git reset --hard HEAD && git pull -f origin master && npm install && gulp server'");
+	cp.execSync(`su runner -c 'cd "${__dirname}" && git reset --hard HEAD && git pull -f origin master && npm install && gulp server'`);
 	process.exit();
 });
 
